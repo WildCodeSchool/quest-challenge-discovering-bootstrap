ea72208700bb3ec8c9327878550407c2dfcd2db7
Jean-François Morin <jeanfrancois.morin@wildcodeschool.com>
2022-03-21 23:38:07 +0100

    add stretched-link + BS docs link

---
+++ _footer.php
@@ -0,0 +1,13 @@
+<footer class="mt-auto">
+    <section class="bg-light p-5">
+        <div class="container text-center">
+            <h2>The css frameworks are very useful</h2>
+            <p>
+                Learning them comes with practice
+            </p>
+        </div>
+    </section>
+    <div class="bg-primary text-white p-2 text-center">
+        &copy; 2022 wilders | images <a href='https://picsum.photos/' target="_blank">@picsum</a>
+    </div>
+</footer>
--- index.php
+++ index.php
@@ -21,22 +21,26 @@
                     [
                         'title' => "Visit Components lake",
                         'text' => 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
-                        'img' => 'https://picsum.photos/id/1011/600/400'
+                        'img' => 'https://picsum.photos/id/1011/600/400',
+                        'link' => 'https://getbootstrap.com/docs/5.1/components/accordion/'
                     ],
                     [
                         'title' => "Facing Cascade Style Sheet",
                         'text' => 'Some quick example text.',
-                        'img' => 'https://picsum.photos/id/1035/600/400'
+                        'img' => 'https://picsum.photos/id/1035/600/400',
+                        'link' => 'https://getbootstrap.com/docs/5.1/customize/css-variables/'
                     ],
                     [
                         'title' => 'Contemplate Form Inputs river',
-                        'text' => 'Make up the bulk of the card\'s content.', 
-                        'img' => 'https://picsum.photos/id/1015/600/400'
+                        'text' => 'Make up the bulk of the card\'s content.',
+                        'img' => 'https://picsum.photos/id/1015/600/400',
+                        'link' => 'https://getbootstrap.com/docs/5.1/forms/overview/'
                     ],
                     [
                         'title' => 'Enjoy Responsive Layout land',
-                        'text' => 'Build on the card title and make up the bulk of the card\'s content.', 
-                        'img' => 'https://picsum.photos/id/103/600/400'
+                        'text' => 'Build on the card title and make up the bulk of the card\'s content.',
+                        'img' => 'https://picsum.photos/id/103/600/400',
+                        'link' => 'https://getbootstrap.com/docs/5.1/layout/breakpoints/'
                     ],
                 ];
                 ?>
@@ -45,11 +49,16 @@
                         <div class="card h-100 shadow border-0">
                             <img src="<?= $content["img"] ?>" class="card-img-top" alt="">
                             <div class="card-body d-flex flex-column">
-                                <h5 class="card-title"><?=$content['title']?></h5>
+                                <h5 class="card-title"><?= $content['title'] ?></h5>
                                 <p class="card-text d-none d-lg-block">
                                     <?= $content["text"] ?>
                                 </p>
-                                <a href="#" class="btn btn-primary mt-auto">Read more</a>
+                                <a href='<?= $content["link"] ?>' 
+                                class="btn btn-primary mt-auto stretched-link"
+                                target="_blank"
+                                >
+                                    Read more
+                                </a>
                             </div>
                         </div>
                     </article>
@@ -57,19 +66,7 @@
             </div>
         </section>
     </main>
-    <footer class="mt-auto">
-        <section class="bg-light p-5">
-            <div class="container text-center">
-                <h2>The css frameworks are very useful</h2>
-                <p>
-                    Learning them comes with practice
-                </p>
-            </div>
-        </section>
-        <div class="bg-primary text-white p-2 text-center">
-            &copy; 2022 wilders | images <a href="https://picsum.photos/" target="_blank">@picsum</a>
-        </div>
-    </footer>
+    <?php include '_footer.php' ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 
